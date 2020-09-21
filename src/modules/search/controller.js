b420f0aa82be1f8ab7dfcb6a5c68827dd1437f2c
Mithun Kamath <callmekatootie@gmail.com>
2020-09-21 21:05:28 +0530

    Measure search execution time

--- src/modules/search/controller.js
+++ src/modules/search/controller.js
@@ -9,7 +9,9 @@ const service = require('./service')
  * Search for users. Returns enriched users
  */
 async function searchUsers (req, res) {
+  console.time('searchusers')
   const result = await esHelper.searchUsers(req.auth, req.body, req.query)
+  console.timeEnd('searchusers')
   injectSearchMeta(req, res, result)
   res.send(result.result)
 }
