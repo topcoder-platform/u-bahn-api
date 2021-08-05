81553f838b99cadbae381040cbb14222f1bd1250
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2021-08-05 13:55:16 +0530

    refresh=true, so that new index is immediately available for seraching

--- src/common/es-helper.js
+++ src/common/es-helper.js
@@ -295,7 +295,7 @@ async function processCreate (resource, entity) {
     type: DOCUMENTS[resource].type,
     id: entity.id,
     body: entity,
-    refresh: 'wait_for'
+    refresh: 'true'
   })
   logger.info(`Insert in Elasticsearch resource ${resource} entity, , ${JSON.stringify(entity, null, 2)}`)
 }
